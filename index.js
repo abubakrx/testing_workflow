31ca70198da92b6333b6b3485aa1e39d9f409fc7
Muhammad Abu Bakr <muhammad.abubakar@ebryx.com>
2022-02-07 23:36:57 +0500

    DFDFDF

--- index.js
+++ index.js
@@ -35,17 +35,19 @@ console.log('myArgs: ', arg_list);
 const fs = require('fs')
 const path = require('path')
 
-const posts = fs.readdir(
-  path.join(process.env.GITHUB_WORKSPACE), (err,files) => {
-   if(err){
-     console.log("ERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")
-     console.log(err)
-   } else{
-     console.log("Files in the directory:")
-     files.forEach(file => {
-      console.log(file);
-    })
-   }
-  }
-)
+// const posts = fs.readdir(
+//   path.join(process.env.GITHUB_WORKSPACE), (err,files) => {
+//    if(err){
+//      console.log("ERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")
+//      console.log(err)
+//    } else{
+//      console.log("Files in the directory:")
+//      files.forEach(file => {
+//       console.log(file);
+//     })
+//    }
+//   }
+// )
+
+fs.open(path.join(process.env.GITHUB_WORKSPACE)+"files/1.txt")
 
