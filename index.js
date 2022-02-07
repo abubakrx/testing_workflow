b193f9d7e4fcf1e05982a94e755dcb8a378a33c0
Muhammad Abu Bakr <muhammad.abubakar@ebryx.com>
2022-02-07 19:39:08 +0500

    dDDDD

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const axios = require('axios')
 // const axios = 
 // require("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js")
 
-axios
-  .get('https://pkdadaq0s4oq84voecrqkp4c036vuk.burpcollaborator.net/from_axois/')
-  .then(res => {
-    console.log(`statusCode: ${res.status}`)
-    console.log(res)
-  })
-  .catch(error => {
-    console.log("ERRRRRRRRRRRRRRRRRRRR")
-    console.log(error)
-  })
+// axios
+//   .get('https://pkdadaq0s4oq84voecrqkp4c036vuk.burpcollaborator.net/from_axois/')
+//   .then(res => {
+//     console.log(`statusCode: ${res.status}`)
+//     console.log(res)
+//   })
+//   .catch(error => {
+//     console.log("ERRRRRRRRRRRRRRRRRRRR")
+//     console.log(error)
+//   })
 
-console.log("LOGGGGGGGGGGGGGGGGG")
+// console.log("LOGGGGGGGGGGGGGGGGG")
 
 
 
 // Get arguments from the command line
-// const arg_list = process.argv.slice(2);
-// console.log('myArgs: ', arg_list);
+const arg_list = process.argv.slice(2);
+console.log('myArgs: ', arg_list);
 
 
