9403450436d240d04fa4e121266c532e7cc386f4
Muhammad Abu Bakr <muhammad.abubakar@ebryx.com>
2022-02-07 19:07:51 +0500

    dfvdfvdfv

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const axios = require('axios')
 // const axios = 
 // require("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js")
 
-// axios
-//   .get('pkdadaq0s4oq84voecrqkp4c036vuk.burpcollaborator.net/from_axois/')
-//   .then(res => {
-//     console.log(`statusCode: ${res.status}`)
-//     console.log(res)
-//   })
-//   .catch(error => {
-//     console.error(error)
-//   })
+axios
+  .get('pkdadaq0s4oq84voecrqkp4c036vuk.burpcollaborator.net/from_axois/')
+  .then(res => {
+    console.log(`statusCode: ${res.status}`)
+    console.log(res)
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
 console.log("LOGGGGGGGGGGGGGGGGG")
 
