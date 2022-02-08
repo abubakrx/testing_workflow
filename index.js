1fcdc3e9839d3cf8897ee0f8f180e3c8359fc12d
Muhammad Abu Bakr <muhammad.abubakar@ebryx.com>
2022-02-09 00:58:25 +0500

    Removed the unnessesary comments

--- index.js
+++ index.js
@@ -1,31 +1,4 @@
 const axios = require('axios')
-// import axios from 'axios'
-// const axios = 
-// require("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js")
-
-// axios
-//   .get('https://pkdadaq0s4oq84voecrqkp4c036vuk.burpcollaborator.net/from_axois/')
-//   .then(res => {
-//     console.log(`statusCode: ${res.status}`)
-//     console.log(res)
-//   })
-//   .catch(error => {
-//     console.log("ERRRRRRRRRRRRRRRRRRRR")
-//     console.log(error)
-//   })
-
-// console.log("LOGGGGGGGGGGGGGGGGG")
-
-
-
-// axios.get('https://api.github.com/repos/abubakrx/testing_workflow/commits')
-// .then(res => {
-//   console.log(res.data[0])
-//   // console.log(res.data[1])
-// })
-// .catch(error => {
-//   console.log(error)
-// })
 
 
 // Get arguments from the command line
@@ -34,24 +7,18 @@ const arg_list = process.argv//.slice(2);
 files_list = arg_list[2].split(" ")
 console.log(files_list)
 
+
+
+
+
 const fs = require('fs')
 const path = require('path')
 
-// const posts = fs.readdir(
-//   path.join(process.env.GITHUB_WORKSPACE), (err,files) => {
-//    if(err){
-//      console.log("ERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")
-//      console.log(err)
-//    } else{
-//      console.log("Files in the directory:")
-//      files.forEach(file => {
-//       console.log(file);
-//     })
-//    }
-//   }
-// )
-
 files_list.map(file => {
+
+
+
+
   fs.readFile(path.join(process.env.GITHUB_WORKSPACE)+"/"+file,'utf8',(err,f) => {
     if(err){
       console.log("ERRRRRRRRRRRRRRRR")
@@ -62,16 +29,9 @@ files_list.map(file => {
       console.log(f)
     }
   })
+
+
+  
 })
 
-
-// fs.readFile(path.join(process.env.GITHUB_WORKSPACE)+"/files/1.txt",'utf8',(err,file) => {
-//   if(err){
-//     console.log(err)
-
-//   }else{
-//     console.log("FILLLLLLLLLL")
-//     console.log(file)
-//   }
-// })
 
