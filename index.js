46f71b40bc808579e2c459964002dc2e068d0d67
Muhammad Abu Bakr <muhammad.abubakar@ebryx.com>
2022-02-08 07:36:50 +0500

    DFDFDF

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const axios = require('axios')
 // Get arguments from the command line
 const arg_list = process.argv//.slice(2);
 // console.log('myArgs: ', arg_list[2]);
-files_list = arg_list[2].splice(" ")
+files_list = arg_list[2].split(" ")
 console.log(files_list)
 
 const fs = require('fs')
